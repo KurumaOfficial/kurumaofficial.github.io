ea4e4252b6d044652ef69373d31f13caf42ac45d
Kuruma <kurumibestwaify@gmail.com>
2026-03-19 22:04:09 +0100

    Update Aleph Studio site content

--- src/data/site-data.js
+++ src/data/site-data.js
@@ -92,7 +92,204 @@ export const DEFAULT_SITE_DATA = {
             "sortOrder": 4
         }
     ],
-    "supporters": [],
+    "supporters": [
+        {
+            "id": "discord-1079398032803643503",
+            "name": "♡ sodochkaオタク♡",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1079398032803643503/a81f481e69a7881168bd8e7bc6c3acca.png?size=256",
+            "sortOrder": 2
+        },
+        {
+            "id": "discord-733668671779766313",
+            "name": "АЛЕКСЕЙ",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/733668671779766313/679677d9f2afd78198438378ae76b2f1.png?size=256",
+            "sortOrder": 3
+        },
+        {
+            "id": "discord-1291298249033908396",
+            "name": "Водяной",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1291298249033908396/bc611dc5e58917006765fddc63e947ff.png?size=256",
+            "sortOrder": 4
+        },
+        {
+            "id": "discord-1359596143129919651",
+            "name": "грустни криперочек 2",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1359596143129919651/8d767be70cd02d4c27c46e972e8f5fa9.png?size=256",
+            "sortOrder": 5
+        },
+        {
+            "id": "discord-1185734375183237180",
+            "name": "Коть",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1185734375183237180/d7eb83ccb16ea22560a35de51fa9c644.png?size=256",
+            "sortOrder": 6
+        },
+        {
+            "id": "discord-1255609844623212736",
+            "name": "Молочный Зубик",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1255609844623212736/17670be50d8d8aca19407d63614bb3d2.png?size=256",
+            "sortOrder": 7
+        },
+        {
+            "id": "discord-1342916631671738430",
+            "name": "Папка хоумлендер",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1342916631671738430/9b3a93b2d0ce6304d20284cc79cdb0b7.png?size=256",
+            "sortOrder": 8
+        },
+        {
+            "id": "discord-1453404424616804549",
+            "name": "Пloxaya_Devo4ka",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1453404424616804549/e90ef9024b61a2d74621f0a8d2412f90.png?size=256",
+            "sortOrder": 9
+        },
+        {
+            "id": "discord-1170330309208842250",
+            "name": "точно пон",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1170330309208842250/b19cca45168ad33f5902302feade8129.png?size=256",
+            "sortOrder": 10
+        },
+        {
+            "id": "discord-1051504786203222117",
+            "name": "AbsoluteAUT",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1051504786203222117/622c1ed4fd81b36214f83750c85c664d.png?size=256",
+            "sortOrder": 11
+        },
+        {
+            "id": "discord-1352212012414795827",
+            "name": "alusiss",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1352212012414795827/a_0cf360373f4d7df391e792a29cccacb3.png?size=256",
+            "sortOrder": 12
+        },
+        {
+            "id": "discord-1442856717959299113",
+            "name": "br4d0ik",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1442856717959299113/15cb2a5079c98049e016d5921a1c1e4f.png?size=256",
+            "sortOrder": 13
+        },
+        {
+            "id": "discord-1124269248190103665",
+            "name": "cerdise",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1124269248190103665/d6e5037776dfceb729c8c06fb28d3f0a.png?size=256",
+            "sortOrder": 14
+        },
+        {
+            "id": "discord-1389546320502722592",
+            "name": "DemoSystem \\\\ Брат Барсика \\\\ EP",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1389546320502722592/77c0560e6293544af04248ddfc5373ee.png?size=256",
+            "sortOrder": 15
+        },
+        {
+            "id": "discord-727201783625285672",
+            "name": "Dobccer",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/727201783625285672/a206722acd404152bdf94d6d2a4f6761.png?size=256",
+            "sortOrder": 16
+        },
+        {
+            "id": "discord-891539316474527744",
+            "name": "Draff3434",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/embed/avatars/5.png",
+            "sortOrder": 17
+        },
+        {
+            "id": "discord-1317475563312644178",
+            "name": "𝗱𝗿𝗲𝗸𝘃𝗲𝘁",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1317475563312644178/2a2cd23be66d5058b2c3c9685db543cc.png?size=256",
+            "sortOrder": 18
+        },
+        {
+            "id": "discord-1132709226167402586",
+            "name": "kalry",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1132709226167402586/85a8db92cd03f33aa66ae193d60657bc.png?size=256",
+            "sortOrder": 20
+        },
+        {
+            "id": "discord-1133014224659021895",
+            "name": "koti4kaa",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1133014224659021895/076f886783635ab4d40ae4228bb6e3f9.png?size=256",
+            "sortOrder": 21
+        },
+        {
+            "id": "discord-1356568559026966654",
+            "name": "logintojoin",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1356568559026966654/06508aeb5e8bc35b0b3ab2dff44bfc00.png?size=256",
+            "sortOrder": 22
+        },
+        {
+            "id": "discord-1470031159600615618",
+            "name": "Neverlifer",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1470031159600615618/aee44bb0c86384cb3c9b1005b80e1cf0.png?size=256",
+            "sortOrder": 23
+        },
+        {
+            "id": "discord-1184124930573475864",
+            "name": "Nomadvorga",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1184124930573475864/04b2e0a482c16dcc801e9db1c804c461.png?size=256",
+            "sortOrder": 24
+        },
+        {
+            "id": "discord-1393592010279227463",
+            "name": "out144fjhs😎",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1393592010279227463/3513c947ee6b49dccf6fffe40a313293.png?size=256",
+            "sortOrder": 25
+        },
+        {
+            "id": "discord-1384921447176343632",
+            "name": "rusl1k",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1384921447176343632/1663567d0c32d208093ab38697143940.png?size=256",
+            "sortOrder": 26
+        },
+        {
+            "id": "discord-1053002738922962945",
+            "name": "S42949",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1053002738922962945/e9af29e112425a5ccc8cc15226bd37b1.png?size=256",
+            "sortOrder": 27
+        },
+        {
+            "id": "discord-1366124388490940528",
+            "name": "Sade",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1366124388490940528/86898f3dff78ef8b3786b73dd7ecfdf7.png?size=256",
+            "sortOrder": 28
+        },
+        {
+            "id": "discord-1265650356570820633",
+            "name": "strange😡",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/1265650356570820633/2c29578643f70e8c9c7c6a49b434fa91.png?size=256",
+            "sortOrder": 29
+        },
+        {
+            "id": "discord-567713040351494174",
+            "name": "ᴡᴇʟᴡᴇʀ_ɢᴀɴ",
+            "role": "Premium",
+            "avatarUrl": "https://cdn.discordapp.com/avatars/567713040351494174/35df3dbca620b7f1dfbad1b84f97998b.png?size=256",
+            "sortOrder": 30
+        }
+    ],
     "socials": {
         "youtube": "https://www.youtube.com/@KurumaBestWaify",
         "discord": "https://discord.gg/3YxB2R6eqQ",
